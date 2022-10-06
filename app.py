b2856c7981a22f94539a6b439dd6d33edfb344b6
CarlosOrtiz94 <51864121+CarlosOrtiz94@users.noreply.github.com>
2022-10-05 19:18:16 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+import sklearn
+
+data = pd.read_csv('clean_titanic.csv')
+modelo = pickle.load(open('modelo.pickle', 'rb'))
+
+# Título
+
+st.title('¿Habrías sobrevivido al Titanic?')
+
+# Exploración inicial
+
+st.header('Exploración inicial')
+
+st.subheader('Los primeros datos')
+st.dataframe(data.head())
+
+st.subheader('Un descriptivo')
+st.dataframe(data.describe())
+
+# Visualización
+
+fig, ax = plt.subplots(1, 4, sharey=True, figsize=(16, 4))
+ax[0].set_ylabel('%')
+
+for idx, col in enumerate(['pclass','sibsp','parch','sex_male']):
+  data[col].value_counts(normalize=True).plot(kind='bar', ax=ax[idx], title=col)  
+plt.show()
+
+st.pyplot(fig)
+
+fig, ax = plt.subplots(1, 4, sharey=True, figsize=(16, 4))
+
+for idx, col in enumerate(['pclass','sibsp','parch','sex_male']):
+  pd.crosstab(data[col], data['survived']).plot(kind='bar', ax=ax[idx], title=col) 
+plt.show()
+
+st.pyplot(fig)
+
+
+# Selección de datos
+
+st.header('Verifica si habrías sobrevivido')
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.header('Características')
+  sexo = st.selectbox('Género', ('M', 'F'))
+  if sexo == 'M':
+    sexo = 1
+  else:
+    sexo = 0
+   
+  par_hijos = st.slider('Número entre padres e hijos', 0, 8)
+  hermanos_esposos = st.slider('Número entre hermanos(as) y esposos(as)', 0, 5)
+  
+with col2:
+  st.header('Boleto')
+  clase = st.selectbox('Clase', (1, 2, 3)
+  edad = st.slider('Edad', 0, 99)
+  fare = st.slider('Disposición a pagar por el boleto', 0, 500)
+
+# Predicción
+
+if st.button('Predecir'):
+  pred = modelo.predict_proba(np.array([[clase, edad, hermanos_esposos, par_hijos, fare, sexo]]))
+  st.text(f'Habrías sobrevivido con una probabilidad de {round(pred[0][1] * 100, 1)}%.')
+else:
+  st.text('Seleccione entre las opciones y oprima Predecir')
+
